f0ac3e2a0c23e1dc41519259ad0859d2132b0292
Anthony Zhao <anthony.zhao@conductorone.com>
2024-05-01 17:11:14 -0700

    lint

--- pkg/connector/connector.go
+++ pkg/connector/connector.go
@@ -6,7 +6,6 @@ import (
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
 
-	"github.com/conductorone/baton-bitbucket/pkg/bitbucket"
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 	"github.com/conductorone/baton-sdk/pkg/annotations"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
